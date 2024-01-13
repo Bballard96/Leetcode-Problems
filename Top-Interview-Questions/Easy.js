7ca80d8d349e98cc8cf7e6e1e6d118c9260511eb
Brendan96 <Brendan.ballard96@yahoo.com>
2024-01-13 17:28:32 -0600

    answer

--- Top-Interview-Questions/Easy.js
+++ Top-Interview-Questions/Easy.js
@@ -19,5 +19,16 @@ Output: 21
 */
 
 var reverse = function(x) {
+  const INT_MIN = Math.pow(-2, 31);
+  const INT_MAX = Math.pow(2, 31) - 1;
 
-}
+  const sign = x < 0 ? -1 : 1;
+  let reversed = parseInt(Math.abs(x).toString().split('').reverse().join('')) * sign;
+
+  // Check for overflow
+  if (reversed < INT_MIN || reversed > INT_MAX) {
+      return 0;
+  }
+
+  return reversed;
+};
