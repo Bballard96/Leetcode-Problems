b7ede6477912398ea235cc786c667a07828f2f68
Brendan Ballard <brendan.ballard96@yahoo.com>
2024-01-14 12:20:01 -0600

    add new question with examples for reverse linked list

--- Top-Interview-Questions/Easy.js
+++ Top-Interview-Questions/Easy.js
@@ -75,3 +75,21 @@ var reverse = function(x) {
 
   return reversed;
 };
+
+
+/* REVERSE LINKED LIST 
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Examples:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Input: head = [1,2]
+Output: [2,1]
+
+Input: head = []
+Output: []
+
+*/
