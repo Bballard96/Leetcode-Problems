f97d44e4b2e5121f6a777e23ac2a3201e4c280b1
Brendan Ballard <brendan.ballard96@yahoo.com>
2024-01-16 20:50:43 -0600

    for loop psuedo code

--- Top-Interview-Questions/Easy.js
+++ Top-Interview-Questions/Easy.js
@@ -122,6 +122,7 @@ Output: [0,1]
 var twoSum = function(nums, target) {
   // create empty array variable
     let answerArr = []
+    // write first for loop
     for (let i = 0; i < nums.length; i++) {
         for ( let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
