87b5b8e01e1fca31be9559acc88214a75e008d76
Brendan Ballard <brendan.ballard96@yahoo.com>
2024-01-16 20:49:15 -0600

    answer for twosum

--- Top-Interview-Questions/Easy.js
+++ Top-Interview-Questions/Easy.js
@@ -120,5 +120,15 @@ Output: [0,1]
 */
 
 var twoSum = function(nums, target) {
-    
+    let answerArr = []
+    for (let i = 0; i < nums.length; i++) {
+        for ( let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                answerArr.push(i)
+                answerArr.push(j)
+            }
+        }
+    }
+    console.log(answerArr)
+    return answerArr
 };
