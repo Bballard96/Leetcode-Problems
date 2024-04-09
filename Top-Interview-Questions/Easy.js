f31c91f82714dc49190ecd8532e96029bae7aa90
Brendan96 <Brendan.ballard96@yahoo.com>
2024-04-08 20:45:35 -0500

    random

--- Top-Interview-Questions/Easy.js
+++ Top-Interview-Questions/Easy.js
@@ -176,6 +176,7 @@ var plusOne = function(digits) {
     let answer = BigInt(digits.join(''));  
     // Increment BigInt by 1
     answer += 1n;  
+    // random thing for commit
     // Convert BigInt to array of numbers
     let myArr = Array.from(String(answer), Number);  
     return myArr;
