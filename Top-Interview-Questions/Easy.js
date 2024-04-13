3a4c36269d62eabc1a05dcad688085e753dad827
Brendan96 <Brendan.ballard96@yahoo.com>
2024-04-13 15:31:20 -0500

    remove test comment

--- Top-Interview-Questions/Easy.js
+++ Top-Interview-Questions/Easy.js
@@ -176,7 +176,6 @@ var plusOne = function(digits) {
     let answer = BigInt(digits.join(''));  
     // Increment BigInt by 1
     answer += 1n;  
-    // test
     // Convert BigInt to array of numbers
     let myArr = Array.from(String(answer), Number);  
     return myArr;
